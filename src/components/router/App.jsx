0c51fa1ad66675ab2a593a32e089380ccd99c0e3
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-10-31 20:31:00 +0100

    Trying out fakeStoreApi

--- src/components/router/App.jsx
+++ src/components/router/App.jsx
@@ -1,19 +1,61 @@
 import { Link } from 'react-router-dom';
 
+import { useEffect, useState } from 'react';
+
+// const fetchData = () => {
+//   const [products, setProducts] = useState(null)
+
+//   useEffect(() => {
+//     fetch('https://fakestoreapi.com/products')
+//     .then((response) => response.json())
+//     .then((jsonData) => console.log(jsonData))
+//   })
+// }
+
 const App = () => {
+  // const { imageURL, error, loading } = fetchImage();
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products/category/electronics')
+      .then((response) => response.json())
+      .then((jsonData) => console.log(jsonData));
+  });
+
+  // if (error) return <p>A network error was encountered</p>;
+  // if (loading) return <p>Loading...</p>;
+
   return (
-    <div>
-      <h1>Hello from the main page of the app!</h1>
-      <p>Here are some examples of links to other pages</p>
-      <nav>
-        <ul>
-          <li>
-            <Link to='profile'>Profile page</Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
+    // imageURL && (
+    <>
+      <h1>An image</h1>
+      {/* <img src={imageURL} alt={'placeholder text'} /> */}
+      <ul>
+        <li>
+          <Link to='profile'>Profile page</Link>
+        </li>
+      </ul>
+    </>
   );
+  // );
 };
 
 export default App;
+
+// const fetchImage = () => {
+//   const [imageURL, setImageURL] = useState(null);
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     fetch('https://jsonplaceholder.typicode.com/photos', { mode: 'cors' })
+//       .then((response) => {
+//         if (!response.ok) throw new Error('server error');
+//         return response.json();
+//       })
+//       .then((response) => setImageURL(response[0].url))
+//       .catch((error) => setError(error))
+//       .finally(() => setLoading(false));
+//   }, []);
+
+//   return { imageURL, error, loading };
+// };
