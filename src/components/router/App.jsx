dc126f3d988addeb013d12858d5031c74d0f041f
Martin Smeder <119725728+martinsmeder@users.noreply.github.com>
2023-11-01 20:45:57 +0100

    Decided on API and sketched out plan for MVP

--- src/components/router/App.jsx
+++ src/components/router/App.jsx
@@ -16,7 +16,9 @@ const App = () => {
   // const { imageURL, error, loading } = fetchImage();
 
   useEffect(() => {
-    fetch('https://fakestoreapi.com/products/category/electronics')
+    fetch(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+    )
       .then((response) => response.json())
       .then((jsonData) => console.log(jsonData));
   });
